728d6049d19b33a751b7a03e7560c4c52aee3d81
Andrej Dudenhefner <mrhaandi@gmail.com>
2021-11-15 15:53:11 +0100

    UBOUNDED fixed decision

--- CM2_UBOUNDED_dec.v
+++ CM2_UBOUNDED_dec.v
@@ -11,7 +11,8 @@ Notation bounded := (CM2.bounded M).
 Notation step := (CM2.step M).
 Notation reaches := (CM2.reaches M).
 
-Definition path k x := map (fun n => if multi_step n x is Some y then y else x) (seq 0 k).
+
+Definition path k x := map (fun n => multi_step n x) (seq 0 k).
 
 Lemma path_length {k x} : length (path k x) = k.
 Proof. by rewrite /path map_length seq_length. Qed.
@@ -19,14 +20,13 @@ Proof. by rewrite /path map_length seq_length. Qed.
 Lemma Config_eq_dec (x y : Config) : {x = y} + {x <> y}.
 Proof. by do ? decide equality. Qed.
 
-Lemma In_pathE K x y : In y (path K x) -> x = y \/ exists k, 0 < k < K /\ multi_step k x = Some y.
+Lemma option_Config_eq_dec (x y : option Config) : {x = y} + {x <> y}.
+Proof. by do ? decide equality. Qed.
+
+Lemma In_pathE K x y : In (Some y) (path K x) -> exists k, k < K /\ multi_step k x = Some y.
 Proof.
-  move=> /in_map_iff [[|k]].
-  { move=> [<-] _. by left. }
-  case Hk: (multi_step (S k) x) => [z|].
-  - move=> [<-] /in_seq ?. right. exists (S k).
-    split; [lia|done].
-  - move=> [? _]. by left.
+  move=> /in_map_iff [k] [<-] /in_seq ?.
+  exists k. split; [lia|done].
 Qed.
 
 Lemma reachesE x y : reaches x y -> clos_refl_trans Config (fun x' y' => step x' = Some y') x y.
@@ -39,109 +39,116 @@ Proof.
   by apply: rt_step.
 Qed.
 
-Lemma let_try2 K x y : multi_step K x = Some y ->
-  In y (path K x) -> forall z, reaches x z -> In z (path K x).
+Lemma reachesI x y : clos_refl_trans Config (fun x' y' => step x' = Some y') x y -> reaches x y.
 Proof.
-  elim: K y; first done.
-  move=> K IH y. rewrite [multi_step _ _]/=.
-  case Hxz: (multi_step K x) => [z|]; last done.
-  move=> Hzy. have -> : S K = K + 1 by lia.
-  rewrite /path seq_app map_app -/(path _ _) /= Hxz.
-  move=> /in_app_iff [|].
-  - move=> /IH.  Hxz => /IH {}IH. 
-  move=> Hy. move: (Hy) => /IH {}IH. rewrite [In y _]/= => - [|].
-  { move=> ? z'. subst y.    } 
-  { done. }
+  elim.
+  - move=> ???. by exists 1.
+  - move=> ?. by exists 0.
+  - move=> ??? _ ? _ ?. apply: reaches_trans; by eassumption.
+Qed.
 
-Lemma let_try2_dups K x y : multi_step K x = Some y ->
-  In y (path K x) -> forall z, reaches x z -> In z (path K x).
+Lemma path_S {k x} y : step x = Some y -> path (S k) x = (Some x) :: (path k y).
 Proof.
-  move=> HK Hy.
-  have /(pigeonhole Config_eq_dec) : incl (path (K+1) x) (path K x).
-  { move=> z. rewrite /path seq_app map_app.
-    move=> /in_app_iff [|]; first done.
-    rewrite /= HK. by move=> [<-|]. }
-  rewrite ?path_length => /(_ ltac:(lia)).
-  move=> /(dup_seq Config_eq_dec).
-  
-  Search (In _ (_ ++ _)). Search (seq _ (_ + _)).  }
+  move=> Hxy. rewrite /path /= -seq_shift map_map.
+  congr cons. apply: map_ext => - ?.
+  by rewrite /= option_bind_iter Hxy.
+Qed.
+
+Lemma path_S' {k x} : path (S k) x = (path k x) ++ [multi_step k x].
+Proof. by rewrite /path seq_S map_app. Qed.
+
+Lemma multi_step_loop_mod {K x k} : multi_step (S K) x = Some x ->
+  multi_step k x = multi_step (k mod (S K)) x.
+Proof.
+  rewrite [in multi_step k x](Nat.div_mod_eq k (S K)).
+  move: (k mod (S K)) => k' Hx. elim: (k / S K).
+  - congr multi_step. lia.
+  - move=> n IH. have ->: S K * S n + k' = S K + (S K * n + k') by lia.
+    by move: Hx => /multi_step_plus ->.
+Qed.
+
+Lemma path_loopE K x : In (multi_step K x) (path K x) -> 
+  forall k, In (multi_step k x) (path K x).
+Proof.
   elim: K x; first done.
   move=> K IH x. rewrite [multi_step _ _]/= option_bind_iter.
-  case Hxz: (step x) => [z|]; last by rewrite iter_None.
-  move=> Hy. move: (Hy) => /IH {}IH. rewrite [In y _]/= => - [|].
-  { move=> ? z'. subst y.    } 
-  { done. }
+  case Hxz: (step x) => [z|].
+  - move=> H. rewrite (path_S z Hxz) /= in H. case: H.
+    + move=> Hzx. have /multi_step_loop_mod : multi_step (S K) x = Some x.
+      { by rewrite /multi_step /= option_bind_iter Hxz. }
+      move=> Hx k. rewrite Hx. apply /in_map_iff. exists (k mod (S K)).
+      split; first done. apply /in_seq.
+      have := Nat.mod_upper_bound k (S K). lia.
+    + rewrite (path_S z Hxz).
+      move=> /IH {}IH [|k]; first by left.
+      rewrite /= option_bind_iter Hxz. right. by apply: IH.
+  - rewrite iter_None. move=> /in_map_iff [k] [Hk] /in_seq HK.
+    move=> k'. have [|Hkk'] : k' < k \/ k <= k' by lia.
+    + move=> ?. apply /in_map_iff. exists k'.
+      split; first done. apply /in_seq. lia.
+    + move: (Hk) => /(multi_step_k_monotone k') ->; [done|right].
+      suff ->: (K = S (K - 1)) by left.
+      move: K HK {IH} => [|K] ?; last by lia.
+      move: Hk. by have ->: k = 0 by lia.
+Qed.
 
-Lemma let_try K x y : multi_step K x = Some y ->
-(In y (path K x) <-> bounded K x).
+
+Lemma path_loopE' K x : In (multi_step K x) (path K x) -> 
+  forall y, reaches x y -> In (Some y) (path K x).
 Proof.
-  elim: K x.
-  { move=> x /= _. split; first done.
-    move=> [[|? L]] /= []; last by lia.
-    move=> _ /(_ x). apply. by apply: reaches_refl. }
-  move=> K IH x. rewrite [multi_step _ _]/= option_bind_iter.
-  case Hxz: (step M x) => [z|]; last by rewrite iter_None.
-  rewrite -/(multi_step _ _) => HK.
-  move: (HK) => /IH {}IH. split.
-  - move=> /In_pathE [].
-    + move=> ?. subst y. exists (path (K+1) x).
-      rewrite path_length. split; first by lia.
-      move=> y' /reachesE /(clos_rt_rt1n Config) []. admit.
-      
-      admit. 
-    admit.
-    + move=> [k [? Hk]].
-      have /IH : In y (path K z).
-      { apply /in_map_iff. exists (k-1). }
-      move=> [L] [? HL]. exists (x::L) => /=.
-      split; first by lia.
-      move=> y' [[|k']]. { move=> [?]. by left. }
-      rewrite /= option_bind_iter Hxz => ?.
-      right. apply: HL. by exists k'.
-  - 
-Admitted.
+  move=> /path_loopE H y [k] H'. move: (H k).
+  by congr In.
+Qed.
+
+Lemma path_noloopI k x :
+  ~ In (multi_step k x) (path k x) -> NoDup (path (k + 1) x).
+Proof.
+  elim: k x.
+  { move=> ??. constructor; [done| constructor]. }
+  move=> k IH x.
+  rewrite path_S' /multi_step in_app_iff /= -/(multi_step k x).
+  move=> /Decidable.not_or.
+  have [|/IH ?] := In_dec option_Config_eq_dec (multi_step k x) (path k x).
+  - move=> /path_loopE /(_ (S k)). tauto.
+  - move=> [?] ?.
+    apply /(NoDup_Add (a := multi_step (S k) x) (l := path (k + 1) x)).
+    + rewrite path_S'.
+      have := Add_app (multi_step (k + 1) x) (path (k + 1) x) [].
+      congr Add.
+      * congr multi_step. lia.
+      * by rewrite app_nil_r.
+    + constructor; first done.
+      have ->: k + 1 = S k by lia.
+      rewrite path_S' in_app_iff /=. tauto.
+Qed.
+
 
 Lemma pointwise_decision K x : {bounded K x} + {not (bounded K x)}.
 Proof.
   case HK: (multi_step K x) => [y|].
-  - have [Hy|Hy] := In_dec Config_eq_dec y (path K x).
-    + left. exists (path K x). rewrite path_length.
-      split; first done.
-      move: (Hy) => /in_map_iff [k].
-      case Hk: (multi_step k x) => [z|]; first last.
-      { move=> [_] /in_seq ?.
-        by move: Hk HK => /(multi_step_k_monotone K) /(_ ltac:(lia)) ->. }
-      move=> [?] /in_seq ?. subst z.
-      move=> z [k'].
-      have [?|?] : k' < K \/ K <= k' by lia.
-      { move=> Hk'. apply /in_map_iff. exists k'.
-        rewrite Hk'. split; first done. apply /in_seq. lia. }
-      have -> : k' = k + (k'-k) by lia.
-      rewrite (Nat.div_mod_eq (k'-k) (K-k)).
-      rewrite (multi_step_plus Hk).
-      have := Nat.mod_upper_bound (k' - k) (K - k) ltac:(lia).
-      move: (_ / _) (_ mod _) => n k'' ?.
-      have : multi_step ((K - k) * n) y = Some y.
-      { rewrite (ltac:(lia) : K = k + (K - k)) (multi_step_plus Hk) in HK.
-        move: (K - k) HK => m Hm. elim: n; first by rewrite Nat.mul_0_r.
-        move=> n IH. by rewrite Nat.mul_succ_r Nat.add_comm (multi_step_plus Hm). }
-      move=> /multi_step_plus -> Hk''. apply /in_map_iff. exists (k+k'').
-      rewrite (multi_step_plus Hk) Hk''. split; first done.
-      apply /in_seq. lia.
+  - have [Hy|Hy] := In_dec option_Config_eq_dec (Some y) (path K x).
+    + left. exists (map (fun oy => if oy is Some y then y else x) (path K x)).
+      split. { by rewrite map_length path_length. }
+      move=> z /path_loopE' => /(_ K).
+      rewrite HK => /(_ Hy) ?.
+      apply /in_map_iff. by exists (Some z).
     + right. move=> [L [? HL]].
-      apply: (pigeonhole Config_eq_dec (path (K+1) x) L).
-      * move=> z /in_map_iff [k] [<-] _. apply: HL.
-        case Hk: (multi_step k x) => [{}z|]; [by exists k | by exists 0].
-      * rewrite path_length. lia.
-      * case: (NoDup_dec Config_eq_dec (path (K + 1) x)); first done.
-        move=> /(dup_seq Config_eq_dec) [[i j]].
-        admit.
-  - left. exists (path K x). rewrite length_path. split; first done.
+      apply: (pigeonhole option_Config_eq_dec (path (K+1) x) (map Some L)).
+      * move=> [z|] /in_map_iff [k] [Hk] /in_seq ?.
+        { apply /in_map_iff. exists z. split; first done.
+          apply: HL. by exists k. }
+        by move: Hk HK => /(multi_step_k_monotone K) /(_ ltac:(lia)) ->.
+      * rewrite map_length path_length. lia.
+      * apply /path_noloopI. by rewrite HK.
+  - left. exists (map (fun oy => if oy is Some y then y else x) (path K x)).
+    split. { by rewrite map_length path_length. }
     move=> y [k]. have [?|?] : k < K \/ K <= k by lia.
-    + move=> Hk. apply /in_map_iff. exists k. rewrite Hk in_seq.
-      split; [done|lia].
+    + move=> Hk. apply /in_map_iff. exists (Some y).
+      split; first done.
+      apply /in_map_iff. exists k. split; first done.
+      apply /in_seq. lia. 
     + by move: HK => /(multi_step_k_monotone k) /(_ ltac:(lia)) ->.
-Admitted.
+Qed.
 
 
 Lemma fixed_decision K : 
